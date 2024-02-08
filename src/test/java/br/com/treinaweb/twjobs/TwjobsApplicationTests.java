0912a2f963b63a65b301654f4014204d70971d1a
Cleyson <cleysonph@gmail.com>
2024-02-08 19:19:17 -0300

    02:02 - Criando o projeto

---
+++ src/test/java/br/com/treinaweb/twjobs/TwjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
