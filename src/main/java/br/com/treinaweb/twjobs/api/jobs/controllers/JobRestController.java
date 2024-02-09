60635d71817453ce2d5dab82b8d3eb82414b533a
Cleyson <cleysonph@gmail.com>
2024-02-09 16:15:25 -0300

    05:13 - Listando todas as vagas

---
+++ src/main/java/br/com/treinaweb/twjobs/api/jobs/controllers/JobRestController.java
@@ -0,0 +1,30 @@
+package br.com.treinaweb.twjobs.api.jobs.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.twjobs.api.jobs.dtos.JobResponse;
+import br.com.treinaweb.twjobs.api.jobs.mappers.JobMapper;
+import br.com.treinaweb.twjobs.core.repositories.JobRepository;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/jobs")
+public class JobRestController {
+
+    private final JobMapper jobMapper;
+    private final JobRepository jobRepository;
+
+    @GetMapping
+    public List<JobResponse> findAll() {
+        return jobRepository.findAll()
+            .stream()
+            .map(jobMapper::toJobResponse)
+            .toList();
+    }
+    
+}
