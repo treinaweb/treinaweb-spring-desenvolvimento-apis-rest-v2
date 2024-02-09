1374a48b4c7509ce81194fc4a4e74548c41f3ce2
Cleyson <cleysonph@gmail.com>
2024-02-08 23:41:31 -0300

    04:11 - Criando a validação de nome único

--- src/main/java/br/com/treinaweb/twjobs/api/skills/dtos/SkillRequest.java
+++ src/main/java/br/com/treinaweb/twjobs/api/skills/dtos/SkillRequest.java
@@ -1,5 +1,6 @@
 package br.com.treinaweb.twjobs.api.skills.dtos;
 
+import br.com.treinaweb.twjobs.core.validators.SkillNameIsUnique;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class SkillRequest {
 
     @NotEmpty
+    @SkillNameIsUnique
     private String name;
     
 }
--- src/main/java/br/com/treinaweb/twjobs/core/repositories/SkillRepository.java
+++ src/main/java/br/com/treinaweb/twjobs/core/repositories/SkillRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.treinaweb.twjobs.core.models.Skill;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
     
 }
