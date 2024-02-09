25de6cb80ea00219c3dae50c24cde6683498adce
Cleyson <cleysonph@gmail.com>
2024-02-08 21:51:47 -0300

    04:09 - Editando uma skill

--- src/main/java/br/com/treinaweb/twjobs/api/skills/controllers/SkillRestController.java
+++ src/main/java/br/com/treinaweb/twjobs/api/skills/controllers/SkillRestController.java
@@ -2,10 +2,12 @@ package br.com.treinaweb.twjobs.api.skills.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,6 +48,18 @@ public class SkillRestController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public SkillResponse create(@Valid @RequestBody SkillRequest skillRequest) {
         var skill = skillMapper.toSkill(skillRequest);
+        skill = skillRepository.save(skill);
+        return skillMapper.toSkillResponse(skill);
+    }
+
+    @PutMapping("/{id}")
+    public SkillResponse update(
+        @PathVariable Long id, 
+        @Valid @RequestBody SkillRequest skillRequest
+    ) {
+        var skill = skillRepository.findById(id)
+            .orElseThrow(SkillNotFoundException::new);
+        BeanUtils.copyProperties(skillRequest, skill, "id");
         skill = skillRepository.save(skill);
         return skillMapper.toSkillResponse(skill);
     }
