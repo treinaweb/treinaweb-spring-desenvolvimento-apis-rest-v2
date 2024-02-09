56e2e9cf2db6dbb9e80fede6c28630891a4b1d5a
Cleyson <cleysonph@gmail.com>
2024-02-08 21:44:38 -0300

    04:08 - Cadastrando uma skill

--- src/main/java/br/com/treinaweb/twjobs/api/skills/controllers/SkillRestController.java
+++ src/main/java/br/com/treinaweb/twjobs/api/skills/controllers/SkillRestController.java
@@ -2,15 +2,21 @@ package br.com.treinaweb.twjobs.api.skills.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.treinaweb.twjobs.api.skills.dtos.SkillRequest;
 import br.com.treinaweb.twjobs.api.skills.dtos.SkillResponse;
 import br.com.treinaweb.twjobs.api.skills.mappers.SkillMapper;
 import br.com.treinaweb.twjobs.core.exceptions.SkillNotFoundException;
 import br.com.treinaweb.twjobs.core.repositories.SkillRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -34,6 +40,14 @@ public class SkillRestController {
         return skillRepository.findById(id)
             .map(skillMapper::toSkillResponse)
             .orElseThrow(SkillNotFoundException::new);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public SkillResponse create(@Valid @RequestBody SkillRequest skillRequest) {
+        var skill = skillMapper.toSkill(skillRequest);
+        skill = skillRepository.save(skill);
+        return skillMapper.toSkillResponse(skill);
     }
     
 }
