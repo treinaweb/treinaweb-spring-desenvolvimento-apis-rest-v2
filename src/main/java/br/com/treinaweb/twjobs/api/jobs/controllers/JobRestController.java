15c1c75d1f43f726a519144f897953609b1a0e8f
Cleyson <cleysonph@gmail.com>
2024-02-20 14:15:00 -0300

    05:14 - Exibindo um Job por id

--- src/main/java/br/com/treinaweb/twjobs/api/jobs/controllers/JobRestController.java
+++ src/main/java/br/com/treinaweb/twjobs/api/jobs/controllers/JobRestController.java
@@ -3,11 +3,13 @@ package br.com.treinaweb.twjobs.api.jobs.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.twjobs.api.jobs.dtos.JobResponse;
 import br.com.treinaweb.twjobs.api.jobs.mappers.JobMapper;
+import br.com.treinaweb.twjobs.core.exceptions.JobNotFoundException;
 import br.com.treinaweb.twjobs.core.repositories.JobRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +27,13 @@ public class JobRestController {
             .stream()
             .map(jobMapper::toJobResponse)
             .toList();
+    }
+
+    @GetMapping("/{id}")
+    public JobResponse findById(@PathVariable Long id) {
+        var job = jobRepository.findById(id)
+            .orElseThrow(JobNotFoundException::new);
+        return jobMapper.toJobResponse(job);
     }
     
 }
