7952c4fe58f2a0277747ca89e0510909f7156d84
Cleyson <cleysonph@gmail.com>
2024-02-20 21:57:48 -0300

    06:20 - Tratando a exceção de MethodArgumentNotValidException

--- src/main/java/br/com/treinaweb/twjobs/api/common/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/treinaweb/twjobs/api/common/handlers/ApiExceptionHandler.java
@@ -1,11 +1,15 @@
 package br.com.treinaweb.twjobs.api.common.handlers;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.treinaweb.twjobs.api.common.dtos.ErrorResponse;
+import br.com.treinaweb.twjobs.api.common.dtos.ValidationErrorResponse;
 import br.com.treinaweb.twjobs.core.exceptions.ModelNotFoundException;
 
 @RestControllerAdvice
@@ -13,9 +17,31 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(ModelNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public ErrorResponse handleModelNotFoundException(ModelNotFoundException exception) {
+    public ErrorResponse handleModelNotFoundException(
+        ModelNotFoundException exception
+    ) {
         return ErrorResponse.builder()
             .message(exception.getLocalizedMessage())
+            .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException exception
+    ) {
+        var errors = exception.getBindingResult().getFieldErrors()
+            .stream()
+            .collect(Collectors.groupingBy(
+                fieldError -> fieldError.getField(),
+                Collectors.mapping(
+                    fieldError -> fieldError.getDefaultMessage(),
+                    Collectors.toList()
+                )
+            ));
+        return ValidationErrorResponse.builder()
+            .message("Validation error")
+            .errors(errors)
             .build();
     }
     
